558e042869abd032ae5badefe1e9325957b2753c
Abhishek <abhishekofficials12@gmail.com>
2024-12-10 16:03:24 +0530

    little functional can add,subtract,multiply,divide 2 terms

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,93 @@ import Digits from "./Digits";
 function App() {
 
   const [values,setValues] = useState("")
-  const [operator,setOperator] = useState(false)
+  const [operator,setOperator] = useState({
+    bool:false,
+    value:""
+  })
+  const [index,setIndex]= useState(0)
+  const [finalans,setFinalans] = useState(0)
+  const [ans,setAns]= useState({
+    a:0,
+    b:0
+  })
   function getVal(val){
 if((val==='+'||val==='-'||val==='*'||val==='%' ||val==='='||val==='.') && values===''){
 alert('values not added')
 }
 
 else if((val==='+'||val==='-'||val==='*'||val==='%' ) && values!=''){
-setOperator(true)
+setOperator({
+value:val,
+  bool:true
+})
 setValues(String(values)+String(val))
+setIndex(index+1)
+setAns({...ans,
+  a:Number(values)
+})
+
 }
 
-// if(){
 
-// }
-
-else if(val==='=' && !operator){
+else if(val==='=' && !operator.bool){
   alert('select any operator')
 }
 else{
-  setValues(String(values)+String(val))
+
+  if((values[index-1]==='+'|| values[index-1]==='-'||values[index-1]==='%'||values[index-1]==="*")&& val==="="){
+alert('select the other value')
+  }
+  else{
+    setValues(String(values)+String(val))
+    setIndex(index+1)
+    if(val==="="){
+      let ind=-1;
+      let num=0;
+      for(let i=0; i<values.length-1;i++){
+        if(values[i]==="+"|| values[i]==="-"||values[i]==="*"||values[i]==="%"){
+          ind=i;
+       }
+        num= values.slice(ind+1)
+      }
+      setAns({
+        ...ans, 
+        b:Number(num)
+      })
+
+    
+    }
+  }
+ 
 }
+
+
     
     console.log(values)
   }
   useEffect(()=>{
-    console.log(values)
-  },[values])
+    console.log(values,ans,index,finalans)
+    switch(operator.value){
+      case '+':    setFinalans(ans.a+ans.b)
+      break
+      case '-' : setFinalans(ans.a-ans.b)
+      break
+      case '*': setFinalans(ans.a*ans.b)
+      break
+      case '%'  : setFinalans(ans.a/ans.b)
+    }
+ 
+  },[values,finalans,ans.a,ans.b])
   return (
     <div className="h-[100vh] bg-gray-100 ">
+      {
+        finalans
+      }
    <p className="bg-gray-400 text-[40px] text-white py-[20px] pl-[37%]">CALCULATOR APP</p>
-   <input type="text" className="border-black border-solid border-[1px] absolute w-[500px] left-[410px] h-[40px] top-[120px] pl-[20px]"  readonly value={values}/>
+   <input type="text" className="border-black border-solid border-[1px] absolute w-[500px] left-[410px] h-[40px] top-[120px] pl-[20px]"  readOnly value={values}/>
    <div className="mt-[80px] ml-[340px] text-[30px] text-gray-800">
    
-   <Digits getVal={getVal} setOperator={setOperator}/>
+   <Digits getVal={getVal} />
 
    </div>
 
--- src/Digits.jsx
+++ src/Digits.jsx
@@ -3,7 +3,7 @@ import Multiply from "./Multiply";
 import Subtract from "./Subtract";
 import Sum from "./Sum";
 
-function Digits({getVal,setOperator}) {
+function Digits({getVal}) {
     return (
         <>
        
@@ -11,20 +11,20 @@ function Digits({getVal,setOperator}) {
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(1)}>1</p>
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(2)}>2</p>
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(3)}>3</p>
-<Sum getVal={getVal} setOperator={setOperator}/>
+<Sum getVal={getVal} />
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(4)}>4</p>
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(5)}>5</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(6)}>6</p>
-<Subtract getVal={getVal} setOperator={setOperator} />
+<Subtract getVal={getVal}  />
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(7)}>7</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(8)}>8</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(9)}>9</p>
-<Multiply getVal={getVal} setOperator={setOperator}/>
+<Multiply getVal={getVal} />
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal('.')}>.</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(0)}>0</p>
 
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal('=')}>=</p>
-<Divide getVal={getVal} setOperator={setOperator}/>
+<Divide getVal={getVal} />
 
 
       </div>
