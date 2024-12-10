067d7d093adae40c90567eb5fe85af0db50ea22e
Abhishek <abhishekofficials12@gmail.com>
2024-12-10 14:39:33 +0530

    worked on input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { useEffect, useState } from "react";
 import Digits from "./Digits";
 
 function App() {
+
+  const [values,setValues] = useState("")
+  function getVal(val){
+
+    setValues(String(values)+String(val))
+    console.log(values)
+  }
+  useEffect(()=>{
+    console.log(values)
+  },[values])
   return (
-    <div className="h-[100%] bg-gray-100">
+    <div className="h-[100vh] bg-gray-100 ">
    <p className="bg-gray-400 text-[40px] text-white py-[20px] pl-[37%]">CALCULATOR APP</p>
-   <div className="mt-[50px] ml-[340px] text-[30px] text-gray-800">
-   <Digits/>
-   <div className="flex gap-[160px] ">
-    
+   <input type="text" className="border-black border-solid border-[1px] absolute w-[500px] left-[410px] h-[40px] top-[120px] pl-[20px]"  readonly value={values}/>
+   <div className="mt-[80px] ml-[340px] text-[30px] text-gray-800">
    
-   
- 
- 
-   </div>
- 
+   <Digits getVal={getVal}/>
+
    </div>
 
     </div>
--- src/Digits.jsx
+++ src/Digits.jsx
@@ -3,32 +3,32 @@ import Multiply from "./Multiply";
 import Subtract from "./Subtract";
 import Sum from "./Sum";
 
-function Digits() {
+function Digits({getVal}) {
     return (
         <>
        
       <div className="grid grid-cols-4 w-[60%] h-[60%] bg-gray-200">
-<p className="border-black border-[1px] border-solid px-[70px] py-[20px]">1</p>
-<p className="border-black border-[1px] border-solid px-[70px] py-[20px]">2</p>
-<p className="border-black border-[1px] border-solid px-[70px] py-[20px]">3</p>
+<p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(1)}>1</p>
+<p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(2)}>2</p>
+<p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(3)}>3</p>
 <Sum />
-<p className="border-black border-[1px] border-solid px-[70px] py-[20px]">4</p>
-<p className="border-black border-[1px] border-solid px-[70px] py-[20px]">5</p>
-<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]">6</p>
-<Subtract className="border-black border-[1px] border-solid"/>
-<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]">7</p>
-<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]">8</p>
-<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]">9</p>
-<Multiply className="border-black border-[1px] border-solid"/>
-<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]">.</p>
-<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]">0</p>
+<p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(4)}>4</p>
+<p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(5)}>5</p>
+<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(6)}>6</p>
+<Subtract />
+<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(7)}>7</p>
+<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(8)}>8</p>
+<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(9)}>9</p>
+<Multiply />
+<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal('.')}>.</p>
+<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(0)}>0</p>
 
-<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]">=</p>
-<Divide className="border-black border-[1px] border-solid"/>
+<p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal('=')}>=</p>
+<Divide />
 
 
       </div>
-      <button className=" border-black border-[1px] border-solid  mt-[50px] ml-[270px] px-[100px] py-[20px] bg-gray-300">☒</button>
+      <button className=" border-black border-[1px] border-solid  mt-[50px] ml-[210px] px-[100px] py-[20px] bg-gray-300">☒</button>
       </>
     );
   }
