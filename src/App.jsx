f822a246a1fcf724526750a0392d3733877bd88a
Abhishek <abhishekofficials12@gmail.com>
2024-12-10 15:06:46 +0530

    validated it completely for now

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@ import Digits from "./Digits";
 function App() {
 
   const [values,setValues] = useState("")
+  const [operator,setOperator] = useState(false)
   function getVal(val){
 if((val==='+'||val==='-'||val==='*'||val==='%' ||val==='='||val==='.') && values===''){
+alert('values not added')
+}
 
+else if((val==='+'||val==='-'||val==='*'||val==='%' ) && values!=''){
+setOperator(true)
+setValues(String(values)+String(val))
 }
 
 // if(){
 
 // }
+
+else if(val==='=' && !operator){
+  alert('select any operator')
+}
 else{
   setValues(String(values)+String(val))
 }
@@ -27,7 +37,7 @@ else{
    <input type="text" className="border-black border-solid border-[1px] absolute w-[500px] left-[410px] h-[40px] top-[120px] pl-[20px]"  readonly value={values}/>
    <div className="mt-[80px] ml-[340px] text-[30px] text-gray-800">
    
-   <Digits getVal={getVal}/>
+   <Digits getVal={getVal} setOperator={setOperator}/>
 
    </div>
 
--- src/Digits.jsx
+++ src/Digits.jsx
@@ -3,7 +3,7 @@ import Multiply from "./Multiply";
 import Subtract from "./Subtract";
 import Sum from "./Sum";
 
-function Digits({getVal}) {
+function Digits({getVal,setOperator}) {
     return (
         <>
        
@@ -11,20 +11,20 @@ function Digits({getVal}) {
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(1)}>1</p>
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(2)}>2</p>
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(3)}>3</p>
-<Sum getVal={getVal}/>
+<Sum getVal={getVal} setOperator={setOperator}/>
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(4)}>4</p>
 <p className="border-black border-[1px] border-solid px-[70px] py-[20px]" onClick={()=>getVal(5)}>5</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(6)}>6</p>
-<Subtract getVal={getVal} />
+<Subtract getVal={getVal} setOperator={setOperator} />
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(7)}>7</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(8)}>8</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(9)}>9</p>
-<Multiply getVal={getVal} />
+<Multiply getVal={getVal} setOperator={setOperator}/>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal('.')}>.</p>
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal(0)}>0</p>
 
 <p className="border-black border-[1px] border-solid  px-[70px] py-[20px]" onClick={()=>getVal('=')}>=</p>
-<Divide getVal={getVal}/>
+<Divide getVal={getVal} setOperator={setOperator}/>
 
 
       </div>
